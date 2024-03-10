5895afa4bdccff99e2fc894629844af7527fa67f
yeyulchoi <yeyulchoi@outlook.com>
2024-03-10 04:51:03 -0400

    turtle racing game

---
+++ main.py
@@ -0,0 +1,49 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collide with cars, game is over.
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        scoreboard.increase_level()
+        car_manager.level_up()
+        player.go_to_start_point()
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-250, 260)
+        self.write(f"Level {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", align="center", font=FONT)
